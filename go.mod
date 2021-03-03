21402ab42e9682cf2aa98c3076128266f1579e8a
Michael Garifullin <michael.garifullin@smartbox.com>
2021-03-03 23:42:43 +0000

    init

---
+++ go.mod
@@ -0,0 +1,13 @@
+module jpi
+
+go 1.16
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/coocood/freecache v1.1.1
+	github.com/labstack/echo/v4 v4.2.0
+	github.com/swaggo/echo-swagger v1.1.0
+	github.com/swaggo/swag v1.7.0 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/sys v0.0.0-20210303074136-134d130e1a04 // indirect
+)
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"github.com/coocood/freecache"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	echoSwagger "github.com/swaggo/echo-swagger"
+	"io/ioutil"
+	_ "jpi/docs"
+	"log"
+	"net/http"
+)
+
+var cache = freecache.NewCache(1 * 1024 * 1024)
+
+// @title JPI app
+// @description This is a jpi management application
+// @version 1.0
+// @host localhost:1323
+// @BasePath /
+func main() {
+	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, Smartbox!")
+	})
+	e.GET("/api", func(c echo.Context) error {
+		return c.String(http.StatusOK, GetWeather())
+	})
+
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Get weather
+// @Summary Get weather
+// @Description Get weather description
+// @Tags todos
+// @Produce json
+// @Success 201 {object}
+// @Router /api [get]
+func GetWeather() string {
+	url := "https://community-open-weather-map.p.rapidapi.com/weather?q=Dublin%2Cie&units=metric"
+	key := []byte("Dublin")
+	cached := getCache(key)
+	if cached != "" {
+		log.Print("hit cache")
+		return cached
+	}
+
+	log.Print("real request")
+
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("x-rapidapi-key", "f76e1ae731msh4629dae9182758fp1a95a4jsnb031d2d37365")
+	req.Header.Add("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	setCache(key, body)
+
+	return string(body)
+}
+
+func getCache(k []byte) string {
+	got, err := cache.Get(k)
+	if err != nil {
+		return ""
+	}
+	return string(got)
+}
+
+func setCache(k []byte, v []byte)  {
+	cache.Set(k, v, 60)
+}
